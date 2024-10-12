4b38b980fc0348da91eaf711b8abb991ac88c306
Lahiru heshan <lahiruheshan454@gmail.com>
2024-10-12 10:22:20 +0530

    new

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,11 +4,17 @@ const Nav = () => {
 
     return (
 
-        <div>
+        <div >
+    <nav class="navbar bg-dark border-bottom border-body " data-bs-theme="dark">
 
-            <Link to="/studenttable" className="btn btn-light">
+    <Link to="/studenttable" className="btn text-light py-3 ">
                 Go To Table Page
             </Link>
+            <Link to="/" className="btn text-light">
+                Go To Register Page
+            </Link>
+    </nav>
+          
         </div>
 
     );
